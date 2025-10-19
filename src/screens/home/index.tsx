5720942ba58df32bb905e1dd75f569ca91747e48
daniel levi <daniellevi000@gmail.com>
2025-10-19 09:29:53 -0300

    feat: :sparkles: adiciona header

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,15 +1,18 @@
+import { AppHeader } from "@/components/ app-header";
 import { useAuthContext } from "@/context/auth-context";
 import type { PrivateStackParamsList } from "@/routes/private-routes";
 import { useNavigation } from "@react-navigation/native";
 import type { StackNavigationProp } from "@react-navigation/stack";
 import { Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 export const Home = () => {
   const navigation =
     useNavigation<StackNavigationProp<PrivateStackParamsList>>();
 
   const { handleLogout } = useAuthContext();
   return (
-    <View className="flex-1 items-center justify-center">
+    <SafeAreaView className="flex-1 bg-background-primary">
+      <AppHeader />
       <Text className="text-black text-3xl items-center justify-center ">
         home
       </Text>
@@ -17,6 +20,6 @@ export const Home = () => {
       <TouchableOpacity onPress={handleLogout}>
         <Text>Sair</Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 };
